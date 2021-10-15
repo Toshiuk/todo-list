8af86f1580b9275687611634da13915353483867
Alessandra Lai <alessandralai@hotmail.com>
2021-10-15 10:34:04 +0100

    change label and fix flexbox

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const addForm = document.querySelector(".add");
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+});
+
 document.querySelector(".source").addEventListener("click", function () {
   let audio = new Audio("assets/sound.mp3");
   audio.play();
